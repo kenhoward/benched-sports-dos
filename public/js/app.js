3a030cc5b13511a8657e03ab7494e5bac5c33948
Kenny Howard <ken.chad.howard@gmail.com>
2016-12-18 00:21:06 -0700

    updating routes so that it will go to draft day view by default - will need to uncomment home route when I actually utilize it

--- public/js/app.js
+++ public/js/app.js
@@ -3,20 +3,15 @@ var app = angular.module('FantasyFootball', ['ngRoute', 'ui.bootstrap']);
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/', {
-      templateUrl: './views/home.html',
-      controller: 'HomeController'
-    })
+    // .when('/', {
+    //   templateUrl: './views/home.html',
+    //   controller: 'HomeController'
+    // })
     .when('/draft-day', {
       templateUrl: './views/draftDay.html',
       controller: 'DraftDayController'
-      // resolve: {
-      //   rosterData: function(RosterService, $route) {
-      //     return RosterService.getRoster($route.current);
-      //   }
-      // }
     })
     .otherwise({
-      redirectTo: '/'
+      redirectTo: '/draft-day'
     });
 })
