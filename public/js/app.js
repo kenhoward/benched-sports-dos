8b2bdcdfaa7dcde8afae40561bfcf8a70764e17a
Kenny Howard <ken.chad.howard@gmail.com>
2017-02-03 21:48:10 -0700

    adding research view and fixing text decoration on navigation

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,16 @@ var app = angular.module('FantasyFootball', ['ngRoute', 'ui.bootstrap']); //, 'n
 
 app.config(function($routeProvider) {
   $routeProvider
-    // .when('/', {
-    //   templateUrl: './views/home.html',
-    //   controller: 'HomeController'
-    // })
+    .when('/', {
+      templateUrl: './views/home.html',
+      controller: 'HomeController'
+    })
     .when('/draft-day', {
       templateUrl: './views/draftDay.html',
       controller: 'DraftDayController'
+    })
+    .when('/research', {
+      templateUrl: './views/research.html'
     })
     .otherwise({
       redirectTo: '/draft-day'
