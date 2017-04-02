3f02df37ee1cce14e9ba0907a137c80e24b9929d
Kenny Howard <ken.chad.howard@gmail.com>
2017-04-01 18:01:16 -0600

    adding in changes made on the draft view

--- client/app/components/draft/draft.controller.js
+++ client/app/components/draft/draft.controller.js
@@ -2,9 +2,56 @@ class DraftController {
     constructor() {
         this.name = 'draft';
 
-        this.qb = {
-            avg: 3
-        }
+        // Temporary //
+        this.qb = [
+            {
+                "player":"Kenny Howard",
+                "tier":3,
+                "bye":4,
+                "avg":5
+            }
+        ];
+        this.rb = [
+            {
+                "player":"Carlos Hyde",
+                "tier":1,
+                "bye":9,
+                "avg":25
+            }
+        ];
+        this.wr = [
+            {
+                "player":"Brandon Marshall",
+                "tier":2,
+                "bye":9,
+                "avg":5
+            }
+        ];
+        this.te = [
+            {
+                "player":"Dennis Pitta",
+                "tier":1,
+                "bye":5,
+                "avg":5
+            }
+        ];
+        this.def = [
+            {
+                "player":"San Francisco 49ers",
+                "tier":4,
+                "bye":4,
+                "avg":2
+            }
+        ];
+        this.kckr = [
+            {
+                "player":"Steven Hauscka",
+                "tier":1,
+                "bye":4,
+                "avg":1
+            }
+        ];
+        // Temporary //
     }
 }
 
